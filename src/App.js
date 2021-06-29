f4091698360652ff066b9adebadfcadbe9b5e114
KhoaDang <dangkhoa1410@gmail.com>
2021-06-29 12:11:57 -0400

    Title Color Changed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
+import {useState} from 'react'
 
 function App() {
-  const data = {
-    title: 'Test',
+  const data = [
+    {
+      title: 'Test1',
     content: 'Lorem is sum ls ar sl mf as d',
     color: 'red'
-  }
+    },
+    {
+      title: 'Test2',
+    content: 'Lorem is sum ls ar sl mf as d',
+    color: 'blue'
+    },
+    {
+      title: 'Test3',
+    content: 'Lorem is sum ls ar sl mf as d',
+    color: 'green'
+    },
+    {
+      title: 'Test4',
+    content: 'Lorem is sum ls ar sl mf as d',
+    color: 'gray'
+    },
+  ]
 
   const clickHandler = e => {
     e.preventDefault();
-    console.log(e.target.id)
+    setColor(e.target.id)
+  }
+
+  const [color,setColor] = useState('black')
+
+  const titleStyle = {
+    color: color
   }
 
   return (
     <div className="container App">
-      <h1>Title</h1>
-      <Card data = {data} clickHandler = {clickHandler}></Card>
-      <Card data = {data}></Card>
-      <Card data = {data}></Card>
-      <Card data = {data}></Card>
+      <h1 style={titleStyle}>Title</h1>
+      {
+        data.map((item) => {
+          return <Card data={item} clickHandler={clickHandler}></Card>
+        })
+      }
+      
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 const Card = props => {
     const {data,clickHandler} = props
+
+    const btnStyle = {
+        backgroundColor: data.color
+    }
     
     return (
         <div className="row mb-5">
@@ -10,7 +14,7 @@ const Card = props => {
                     <div className="card-body">
                         <h5 className="card-title">{data.title}</h5>
                         <p className="card-text">{data.content}</p>
-                        <a id={data.color} href="#" className="btn btn-primary" onClick={clickHandler}>Click here</a>
+                        <a id={data.color} style = {btnStyle} href="#" className="btn btn-primary" onClick={clickHandler}>Click here</a>
                     </div>
                 </div>
             </div>
